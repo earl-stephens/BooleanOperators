b330ae44da5018e77eef61574ce990a206ee249d
earl-stephens <stephensearl@hotmail.com>
2022-01-10 23:13:23 -0700

    Demonstrate boolean and operator

--- src/application/App.java
+++ src/application/App.java
@@ -3,8 +3,32 @@ package application;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// ==, !=, !, 
+		// && is and
+		// || is or
+		// & and | are bitwise operators
+		
+		boolean isRaining = true;
+		boolean haveUmbrella = true;
+		
+		boolean takeUmbrella = false;
+		
+		/* replace this with:
+		if(isRaining) {
+			if(haveUmbrella) {
+				takeUmbrella = true;
+			} 
+		} 
+		*/
+		
+		if(isRaining && haveUmbrella) {
+			takeUmbrella = true;
+		}
+		
+		//can also do
+		takeUmbrella = isRaining && haveUmbrella ? true : false;
 
+		System.out.println(takeUmbrella);
 	}
 
 }
